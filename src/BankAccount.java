d46bb4a5f3251cda69054c4e8f7d4cd6a73f3f22
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-13 22:36:52 +0600

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BankAccount {
+import java.util.Random;
+
+public class BankAccount {
+     private int amount;
+   double putMoney;
+    int getMoney;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void deposit(Integer putMoney){
+       amount = amount + putMoney;
+
+   }
+   public  void withDraw(Integer getMoney)throws LimitException {
+       if (amount < getMoney){
+           throw new LimitException("твой счет :" + amount, amount);
+       }
+       else {
+           amount = amount - getMoney;
+       }
+   }
+
 }
