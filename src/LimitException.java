f220c8380b87f8989ecd234088f6c8fd028431ca
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-13 22:37:20 +0600

    Update LimitException.java

--- src/LimitException.java
+++ src/LimitException.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class LimitException {
+public class LimitException extends Exception {
+
+   private int remainingAmount;
+
+    public LimitException(String message,int remainingAmount){
+        super(message);
+        this.remainingAmount = remainingAmount;
+    }
+
+    public Integer getRemainingAmount() {
+        return remainingAmount;
+    }
 }
